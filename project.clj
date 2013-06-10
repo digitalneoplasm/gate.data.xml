19af9e41f7af98b7026331d455270563ac33b690
Daniel R. Schlegel <drschleg@buffalo.edu>
2013-06-10 16:33:16 -0400

    Initial Version.

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
 (defproject gate.data.xml "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A Clojure library for parsing XML files created by GATE - the General Architecture for Text Engineering."
   :dependencies [[org.clojure/clojure "1.5.1"]])
